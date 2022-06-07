bc3fa2c4a88eadb1bbfb67eadffb315ddea2dd32
ren132710 <ren132710@gmail.com>
2022-06-07 16:29:47 -0500

    adjust cypress config

--- client/cypress.config.js
+++ client/cypress.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://localhost:1234',
-    viewport: 'macbook-13',
+    viewport: 'ipad-2',
     // viewportWidth: 700,
     // viewportHeight: 900,
     setupNodeEvents(on, config) {
