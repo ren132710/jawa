10567a9bc0ba5631211792041caa63ebbc13015e
ren132710 <ren132710@gmail.com>
2022-06-07 11:01:02 -0500

    backup test data

---
+++ client/data/places.js
@@ -0,0 +1,32 @@
+;[
+  {
+    id: '0498724f-63ce-4b17-81d3-9b3fbd4eb443',
+    location: 'stockholm',
+    lat: 59.334591,
+    long: 18.06324,
+  },
+  {
+    id: '8f38cdb4-ba91-444a-a121-48e6ad26e751',
+    location: 'boston',
+    lat: 42.361145,
+    long: -71.057083,
+  },
+  {
+    id: '90f3d018-bbd3-45be-9c11-debbff73fb6c',
+    location: 'san francisco',
+    lat: 37.733795,
+    long: -122.446747,
+  },
+  {
+    id: '6b819c6d-c8d4-4f2a-94c1-6eec48c6d8c8',
+    location: 'montreal',
+    lat: 45.508888,
+    long: -73.561668,
+  },
+  {
+    id: 'c9ae7c46-81e4-4c9d-a933-bb3c8d14fc87',
+    location: 'new york',
+    lat: 40.7306,
+    long: -73.9352,
+  },
+]
