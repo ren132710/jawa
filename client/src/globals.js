9f05bef58d454f7255a1a18a721a58ba23455c42
ren132710 <ren132710@gmail.com>
2022-07-06 21:46:42 -0500

    responsive using margin auto

--- client/src/globals.js
+++ client/src/globals.js
@@ -9,5 +9,4 @@ export const DEFAULT_PLACES = [
   { id: '90f3d018-bbd3-45be-9c11-debbff73fb6c', location: 'san francisco', lat: 37.7749, long: -122.4194 },
   { id: '6b819c6d-c8d4-4f2a-94c1-6eec48c6d8c8', location: 'montreal', lat: 45.5017, long: -73.5673 },
   { id: 'c9ae7c46-81e4-4c9d-a933-bb3c8d14fc87', location: 'new york', lat: 40.7128, long: -74.006 },
-  { id: '0498724f-63ce-4b17-81d3-9b3fbd4eb443', location: 'stockholm', lat: 59.3293, long: 18.0686 },
 ]
