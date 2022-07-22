9111567c85d406c7f61b53b9fe2a485c01dbd45b
ren132710 <ren132710@gmail.com>
2022-07-22 10:15:16 -0500

    comments

--- client/src/dictionary.js
+++ client/src/dictionary.js
@@ -1,3 +1,8 @@
+/**
+ * Proof of concept
+ * Would need to expand dictionary to include all labels
+ */
+
 const DICTIONARY = {
   1: { en: 'Weather at your places', fr: 'Météo à vos places', sv: 'Väder på dina platser' },
   2: { en: 'Current Weather', fr: 'Météo Actuelle', sv: 'Vädret Just Nu' },
