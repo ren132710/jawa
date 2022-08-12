f7d7c30b646f137ac0f4f271d1a0381f032a8309
ren132710 <ren132710@gmail.com>
2022-08-12 14:34:09 -0500

    add label translations

--- client/src/dictionary.js
+++ client/src/dictionary.js
@@ -4,10 +4,20 @@
  */
 
 const DICTIONARY = {
-  1: { en: 'Weather at your places', fr: 'Météo à vos places', sv: 'Väder på dina platser' },
+  1: { en: 'Weather at your places', fr: 'Météo à vos lieux', sv: 'Väder på dina platser' },
   2: { en: 'Current Weather', fr: 'Météo Actuelle', sv: 'Vädret Just Nu' },
   3: { en: 'Forecast', fr: 'Prévisions', sv: 'Prognos' },
   4: { en: 'Hourly Weather', fr: 'Heure par Heure', sv: 'Timmar' },
+  5: { en: 'New Place', fr: 'Noveau Lieu', sv: 'Ny Plats' },
+  6: { en: 'Feels Like', fr: 'T. Ressentie', sv: 'Känns som' },
+  7: { en: 'Precip', fr: 'Précip', sv: 'Nederbörd' },
+  8: { en: 'Visibility', fr: 'Visibilité', sv: 'Sikt' },
+  9: { en: 'UV Index', fr: 'Indice UV', sv: 'UV-index' },
+  10: { en: 'Humidity ', fr: 'Humidité', sv: 'Luftfuktighet' },
+  11: { en: 'Wind', fr: 'Vent', sv: 'Vind' },
+  12: { en: 'Dew Point', fr: 'Point de rosée', sv: 'Daggpunkt' },
+  13: { en: 'Sunrise', fr: 'Lever du soleil', sv: 'Soluppgång' },
+  14: { en: 'Sunset', fr: 'Coucher du soleil', sv: 'Solnedgång' },
 }
 
 export function getTranslation(index, lang) {
