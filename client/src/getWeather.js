ef800565d3a357778f65c1652ef8b34a0bca75e0
ren132710 <ren132710@gmail.com>
2022-08-16 13:24:46 -0500

    when 3001 blocked by LAN, use 8080

--- client/src/getWeather.js
+++ client/src/getWeather.js
@@ -1,11 +1,10 @@
 import axios from 'axios'
-const AXIOS_TIMEOUT = process.env.AXIOS_TIMEOUT || 10000 //allow time for cloud server container to wake up
+const AXIOS_TIMEOUT = process.env.AXIOS_TIMEOUT || 10000 //allow time for cloud containers to wake up
 const SERVER = process.env.JAWA_SERVER || 'localhost'
-const PORT = process.env.JAWA_PORT || '3001'
-// 'my-app-server.herokuapp.com' || 'localhost:3001'
-const JAWA_SERVER = process.env.CLOUD_JAWA_SERVER || `${SERVER}:${PORT}`
+const PORT = '8080' //when 3001 is blocked by local network, use 8080
+const JAWA_SERVER = process.env.CLOUD_JAWA_SERVER || `${SERVER}:${PORT}` // 'my-app-server.herokuapp.com' || 'localhost:3001'
 const URL = process.env.NODE_ENV === 'production' ? `https://${JAWA_SERVER}/weather` : `http://${JAWA_SERVER}/weather`
-
+console.log('URL: ', URL)
 /**
  * OpenWeather
  * @param {string} lat: latitude, required by OpenWeather
