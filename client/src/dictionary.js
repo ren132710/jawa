d68ece8b6dd4b67b9a09588f6f115b22b2c933cc
ren132710 <ren132710@gmail.com>
2022-09-30 21:24:13 -0500

    comments

--- client/src/dictionary.js
+++ client/src/dictionary.js
@@ -1,20 +1,20 @@
-import { qs, qsa } from './domUtils.js'
+import {qs, qsa} from './domUtils.js'
 
 const DICTIONARY = {
-  1: { en: 'Weather at your places', fr: 'Météo à vos lieux', sv: 'Väder på dina platser' },
-  2: { en: 'Current Weather', fr: 'Météo Actuelle', sv: 'Vädret Just Nu' },
-  3: { en: 'Forecast', fr: 'Prévisions', sv: 'Prognos' },
-  4: { en: 'Hourly Weather', fr: 'Prévisions Horaires', sv: 'Timmar' },
-  5: { en: 'New Place', fr: 'Nouveau Lieu', sv: 'Ny Plats' },
-  6: { en: 'Feels like', fr: 'Ressenti', sv: 'Känns som' },
-  7: { en: 'Precip', fr: 'Précip', sv: 'Nederbörd' },
-  8: { en: 'Visibility', fr: 'Visibilité', sv: 'Sikt' },
-  9: { en: 'UV Index', fr: 'Indice UV', sv: 'UV-index' },
-  10: { en: 'Humidity', fr: 'Humidité', sv: 'Luftfuktighet' },
-  11: { en: 'Wind', fr: 'Vent', sv: 'Vind' },
-  12: { en: 'Dew Point', fr: 'Point de rosée', sv: 'Daggpunkt' },
-  13: { en: 'Sunrise', fr: 'Lever du soleil', sv: 'Soluppgång' },
-  14: { en: 'Sunset', fr: 'Coucher du soleil', sv: 'Solnedgång' },
+  1: {en: 'Weather at your places', fr: 'Météo à vos lieux', sv: 'Väder på dina platser'},
+  2: {en: 'Current Weather', fr: 'Météo Actuelle', sv: 'Vädret Just Nu'},
+  3: {en: 'Forecast', fr: 'Prévisions', sv: 'Prognos'},
+  4: {en: 'Hourly Weather', fr: 'Prévisions Horaires', sv: 'Timmar'},
+  5: {en: 'New Place', fr: 'Nouveau Lieu', sv: 'Ny Plats'},
+  6: {en: 'Feels like', fr: 'Ressenti', sv: 'Känns som'},
+  7: {en: 'Precip', fr: 'Précip', sv: 'Nederbörd'},
+  8: {en: 'Visibility', fr: 'Visibilité', sv: 'Sikt'},
+  9: {en: 'UV Index', fr: 'Indice UV', sv: 'UV-index'},
+  10: {en: 'Humidity', fr: 'Humidité', sv: 'Luftfuktighet'},
+  11: {en: 'Wind', fr: 'Vent', sv: 'Vind'},
+  12: {en: 'Dew Point', fr: 'Point de rosée', sv: 'Daggpunkt'},
+  13: {en: 'Sunrise', fr: 'Lever du soleil', sv: 'Soluppgång'},
+  14: {en: 'Sunset', fr: 'Coucher du soleil', sv: 'Solnedgång'},
 }
 
 function getTranslation(index, lang) {
@@ -22,11 +22,15 @@ function getTranslation(index, lang) {
 }
 
 export function setTranslations(lang) {
-  // set language for main page
+  // set language for search input placeholder
   qs('[data-place-search]').placeholder = getTranslation(1, lang)
+
+  // set language for main page section titles
   qs('[data-dictionary="2"]').textContent = getTranslation(2, lang)
   qs('[data-dictionary="3"]').textContent = getTranslation(3, lang)
   qs('[data-dictionary="4"]').textContent = getTranslation(4, lang)
+
+  // set language for current weather container
   qs('[data-dictionary="5"]').textContent = getTranslation(5, lang)
   qs('[data-dictionary="6"]').textContent = getTranslation(6, lang)
   qs('[data-dictionary="7"]').textContent = getTranslation(7, lang)
