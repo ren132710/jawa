34973d4094da113220bb43980ae80f77989019cd
ren132710 <ren132710@gmail.com>
2022-12-16 14:28:17 -0500

    Translations

--- client-react/src/utils/translations.js
+++ client-react/src/utils/translations.js
@@ -1,15 +1,27 @@
-const Translations = {
+const TRANSLATIONS = {
   1: {
-    en: 'Current Weather',
-    fr: 'Météo Actuelle',
-    sv: 'Vädret Just Nu',
+    en: 'Weather at your places',
+    fr: 'Météo à vos lieux',
+    sv: 'Väder på dina platser',
   },
-  2: { en: 'Feels like', fr: 'Ressenti', sv: 'Känns som' },
-  3: { en: 'Humidity', fr: 'Humidité', sv: 'Luftfuktighet' },
-  4: { en: 'Wind', fr: 'Vent', sv: 'Vind' },
-  5: { en: 'Visibility', fr: 'Visibilité', sv: 'Sikt' },
+  2: { en: 'Current Weather', fr: 'Météo Actuelle', sv: 'Vädret Just Nu' },
+  3: { en: 'Forecast', fr: 'Prévisions', sv: 'Prognos' },
+  4: { en: 'Hourly Weather', fr: 'Prévisions Horaires', sv: 'Timmar' },
+  5: { en: 'New Place', fr: 'Nouveau Lieu', sv: 'Ny Plats' },
+  6: { en: 'Feels like', fr: 'Ressenti', sv: 'Känns som' },
+  7: { en: 'Precip', fr: 'Précip', sv: 'Nederbörd' },
+  8: { en: 'Visibility', fr: 'Visibilité', sv: 'Sikt' },
+  9: { en: 'UV Index', fr: 'Indice UV', sv: 'UV-index' },
+  10: { en: 'Humidity', fr: 'Humidité', sv: 'Luftfuktighet' },
+  11: { en: 'Wind', fr: 'Vent', sv: 'Vind' },
+  12: { en: 'Dew Point', fr: 'Point de rosée', sv: 'Daggpunkt' },
+  13: { en: 'Sunrise', fr: 'Lever du soleil', sv: 'Soluppgång' },
+  14: { en: 'Sunset', fr: 'Coucher du soleil', sv: 'Solnedgång' },
 };
 
-export default function getTranslation(index, lang) {
-  return Translations[index][lang];
+function getTranslation(index, lang) {
+  return TRANSLATIONS[index][lang];
 }
+q;
+
+console.log(getTranslation(1, 'en')); // Weather at your places
