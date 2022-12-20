3dc45e22550bc0844999633b2a970b6c3980e6fa
ren132710 <ren132710@gmail.com>
2022-12-20 14:28:47 -0500

    extract WeatherContext axios call into hook

--- client-react/src/contexts/WeatherContext.jsx
+++ client-react/src/contexts/WeatherContext.jsx
@@ -1,10 +1,6 @@
-import React, { useContext, useState, useEffect, useMemo } from 'react';
+import React, { useContext, useMemo } from 'react';
 import PropTypes from 'prop-types';
-import axios from 'axios';
-
-const TIMEOUT = import.meta.env.VITE_AXIOS_TIMEOUT;
-const WEATHER_SERVER = import.meta.env.VITE_JAWA_SERVER;
-const URL = `https://${WEATHER_SERVER}/weather`;
+import useJawaWeather from '../hooks/useJawaWeather';
 
 // 1. create the contexts
 const WeatherDataContext = React.createContext();
@@ -33,90 +29,9 @@ export function useWeatherAPI() {
 
 // 3. define the provider and delegate value props to the contexts
 export default function WeatherProvider({ children }) {
-  const [isLoading, setIsLoading] = useState(false);
-  const [isError, setIsError] = useState(false);
-  const [places, setPlaces] = useState([]);
-  const [weatherData, setWeatherData] = useState([]);
+  const { isLoading, isError, weatherData, setPlaces } = useJawaWeather();
 
   console.log('WeatherProvider rendered!');
-
-  // TODO: fetch user prefs from prefsContext
-
-  useEffect(() => {
-    // on page initialization, wait until places state is set before initial render
-    if (!places) return;
-
-    let ignore = false;
-
-    const fetchWeather = async () => {
-      setIsLoading(true);
-      const results = await getPlacesWeather(places);
-      setWeatherData(results);
-      setIsLoading(false);
-    };
-
-    if (!ignore) fetchWeather();
-
-    // prevent setting component state when component is unmounted
-    // (i.e., user navigates away from page before fetch completes)
-    return () => {
-      ignore = true;
-    };
-
-    // only need to run useEffect when places changes
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [places]);
-
-  // TODO: pass favorite flag to server so it can be included in the response object
-  /**
-   * The Jawa weather server uses the OpenWeather API
-   * @param {string} lat: latitude, required by OpenWeather
-   * @param {string} long: longitude, required by OpenWeather
-   * @param {string} units: (imperial | metric ), required by OpenWeather
-   * @param {string} lang: (en | fr | sv ), OpenWeather option
-   * @param {string} id:
-   *   - used for adding/deleting places from localStorage
-   *   - pass to server so server can include in the response object
-   * @param {string} location:
-   *  - location is not provided by OpenWeather
-   *  - pass to server so server can include in the response object
-   */
-
-  async function getPlacesWeather() {
-    const promises = [];
-
-    places.forEach((place) => {
-      const params = {
-        lat: place.lat,
-        long: place.long,
-        units: place.units,
-        lang: place.lang,
-        id: place.id,
-        location: place.location,
-      };
-      const promise = getWeather(params);
-      promises.push(promise);
-    });
-
-    const results = await Promise.all(promises);
-    return results;
-  }
-
-  async function getWeather(params) {
-    setIsError(false);
-    console.log('URL: ', URL);
-
-    try {
-      const response = await axios.get(URL, {
-        params,
-        timeout: TIMEOUT,
-      });
-      return response.data;
-    } catch (error) {
-      setIsError(true);
-      console.log('ERROR:', error);
-    }
-  }
 
   const dataContextValue = useMemo(() => {
     return { isLoading, isError, weatherData };
---
+++ client-react/src/hooks/useJawaWeather.js
@@ -0,0 +1,95 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const TIMEOUT = import.meta.env.VITE_AXIOS_TIMEOUT;
+const WEATHER_SERVER = import.meta.env.VITE_JAWA_SERVER;
+const URL = `https://${WEATHER_SERVER}/weather`;
+
+export default function useJawaWeather() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+  const [places, setPlaces] = useState([]);
+  const [weatherData, setWeatherData] = useState([]);
+
+  console.log('useJawaHook called!');
+
+  useEffect(() => {
+    // on page initialization, wait until places state is set before initial render
+    if (!places) return;
+
+    let ignore = false;
+
+    const fetchWeather = async () => {
+      setIsLoading(true);
+      const results = await getPlacesWeather(places);
+      setWeatherData(results);
+      setIsLoading(false);
+    };
+
+    if (!ignore) fetchWeather();
+
+    // prevent setting component state when component is unmounted
+    // (i.e., user navigates away from page before fetch completes)
+    return () => {
+      ignore = true;
+    };
+
+    // only need to run useEffect when places changes
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [places]);
+
+  /**
+   * The Jawa weather server uses the OpenWeather API
+   * @param {string} lat: latitude, required by OpenWeather
+   * @param {string} long: longitude, required by OpenWeather
+   * @param {string} units: (imperial | metric ), required by OpenWeather
+   * @param {string} lang: (en | fr | sv ), OpenWeather option
+   * @param {string} id:
+   *   - used for adding/deleting places from localStorage
+   *   - pass to server so server can include in the response object
+   * @param {string} location:
+   *  - location is not provided by OpenWeather
+   *  - pass to server so server can include in the response object
+   * @param {boolean} favorite:
+   * - favorite is not provided by OpenWeather
+   * - used to filtering places before saving to localStorage
+   * - pass to server so server can include in the response object
+   */
+
+  async function getPlacesWeather() {
+    const promises = [];
+
+    places.forEach((place) => {
+      const params = {
+        lat: place.lat,
+        long: place.long,
+        units: place.units,
+        lang: place.lang,
+        id: place.id,
+        location: place.location,
+        favorite: place.favorite,
+      };
+      const promise = getWeather(params);
+      promises.push(promise);
+    });
+
+    const results = await Promise.all(promises);
+    return results;
+  }
+
+  async function getWeather(params) {
+    setIsError(false);
+
+    try {
+      const response = await axios.get(URL, {
+        params,
+        timeout: TIMEOUT,
+      });
+      return response.data;
+    } catch (error) {
+      setIsError(true);
+      console.log('ERROR:', error);
+    }
+  }
+  return { isLoading, isError, weatherData, setPlaces };
+}
