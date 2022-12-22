497b5b4219be76ce1ed91b18d3077e70f45c7566
ren132710 <ren132710@gmail.com>
2022-12-22 14:09:30 -0500

    conditional rendering

--- client-react/src/components/Main.jsx
+++ client-react/src/components/Main.jsx
@@ -3,34 +3,45 @@ import { useWeatherData } from '../contexts/WeatherContext';
 export default function Main() {
   const { weatherData, isLoading, isError } = useWeatherData();
 
-  // TODO: if isLoading=true, blur the page
-  // TODO: decompose Main into Current, Daily, Hourly
+  console.log('weatherData from Main', weatherData);
+  console.log('isLoading from Main', isLoading);
+  console.log('isError from Main', isError);
 
-  const weather = weatherData[0];
+  // TODO: decompose Main into Current, Daily, Hourly
 
   return (
     <div>
       <h2>Current Weather</h2>
-      {isLoading && <p>Loading...</p>}
-      {isError && <p>Error!</p>}
-      {weather && (
+      {isError ? (
+        <div style={{ padding: '.5rem', color: 'red' }}>
+          Problem fetching weather...
+        </div>
+      ) : (
         <div>
-          <p>Location: {weather.coordinates.location}</p>
-          <p>Currently: {weather.current.description}</p>
-          <p>Temp: {weather.current.temp}</p>
-          <p>Feels like: {weather.current.feelsLike}</p>
-          <p>High: {weather.current.high}</p>
-          <p>Low: {weather.current.low}</p>
-          <p>Humidity: {weather.current.humidity}</p>
-          <p>Wind speed: {weather.current.windSpeed}</p>
-          <p>Wind direction: {weather.current.windDirection}</p>
-          <img
-            src={`http://openweathermap.org/img/wn/${weather.current.icon}.png`}
-            width="50"
-            height="50"
-            alt="clear sky"
-            data-card-icon
-          />
+          {isLoading ? (
+            <div
+              style={{
+                padding: '.5rem',
+              }}
+            >
+              Loading...
+            </div>
+          ) : (
+            <>
+              <p>Location: {weatherData[0]?.coordinates.location}</p>
+              <p>Currently: {weatherData[0]?.current.description}</p>
+              <p>Temp: {weatherData[0]?.current.temp}</p>
+              <p>Wind Speed: {weatherData[0]?.current.windSpeed}</p>
+              <p>Wind Direction: {weatherData[0]?.current.windDirection}</p>
+              <img
+                src={`http://openweathermap.org/img/wn/${weatherData[0]?.current.icon}.png`}
+                width="50"
+                height="50"
+                alt="clear sky"
+                data-card-icon
+              />
+            </>
+          )}
         </div>
       )}
     </div>
