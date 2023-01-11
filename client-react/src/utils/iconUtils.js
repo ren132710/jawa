28b71e7f9fb7b2dd861508a67beb1ca46de78d5d
ren132710 <ren132710@gmail.com>
2023-01-11 14:25:28 -0500

    add link to openweather doc

--- client-react/src/utils/iconUtils.js
+++ client-react/src/utils/iconUtils.js
@@ -1,5 +1,6 @@
 export default function getIconUrl(iconKey, { size = '' } = {}) {
   let sizeKey = '';
+  // see: https://openweathermap.org/weather-conditions#How-to-get-icon-URL
   if (size === 'large') sizeKey = '@4x';
   if (size === 'medium') sizeKey = '@2x';
   return `http://openweathermap.org/img/wn/${iconKey}${sizeKey}.png`;
