deb8cc01fd51ae159eaade16afda1712f8a06bd5
ren132710 <ren132710@gmail.com>
2023-01-20 13:24:11 -0500

    inline vitest tests

--- client-react/src/utils/units.js
+++ client-react/src/utils/units.js
@@ -6,3 +6,22 @@ const UnitOfMeasure = {
 export default function getUnitOfMeasure(system, category) {
   return UnitOfMeasure[system][category];
 }
+
+/**
+ * unit tests
+ */
+
+if (import.meta.vitest) {
+  const { describe, it, expect } = import.meta.vitest;
+
+  describe('#getUnitOfMeasure', () => {
+    it('should return the correct uom for the given system and category', () => {
+      expect(getUnitOfMeasure('imperial', 'temp')).toBe('F');
+      expect(getUnitOfMeasure('imperial', 'distance')).toBe('mi');
+      expect(getUnitOfMeasure('imperial', 'velocity')).toBe('mph');
+      expect(getUnitOfMeasure('metric', 'temp')).toBe('C');
+      expect(getUnitOfMeasure('metric', 'distance')).toBe('km');
+      expect(getUnitOfMeasure('metric', 'velocity')).toBe('kph');
+    });
+  });
+}
--- client-react/vite.config.js
+++ client-react/vite.config.js
@@ -10,20 +10,25 @@ import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
+  define: {
+    'import.meta.vitest': 'undefined', // ignore inline vitest code for builds
+  },
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      '@': path.resolve(__dirname, './src'), // enables configured aliases from jsconfig.json
     },
   },
   css: {
     modules: {
       localsConvention: 'camelCase',
-      generateScopedName: '[local]_[hash:base64:3]',
+      generateScopedName: '[local]_[hash:base64:3]', // define the number of characters in the hash
     },
   },
   test: {
+    includeSource: ['src/**/*.{js,ts}'], // so vitest can find inline vitest code
     coverage: {
       provider: 'c8', // or 'istanbul'
+      reporter: ['text', 'text-summary', 'html'], // specify coverage output types
     },
   },
 });
