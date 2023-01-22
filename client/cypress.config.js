8967d87e0be78e9d27edca876ba55383457551f9
ren132710 <ren132710@gmail.com>
2023-01-21 22:39:17 -0500

    video and screenshots off

--- client/cypress.config.js
+++ client/cypress.config.js
@@ -1,15 +1,15 @@
-const { defineConfig } = require('cypress')
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
     chromeWebSecurity: false,
     baseUrl: 'http://localhost:1234',
     viewport: 'ipad-2',
-    // viewportWidth: 700,
-    // viewportHeight: 900,
+    screenshotOnRunFailure: false,
+    video: false,
     setupNodeEvents(on, config) {
-      console.log('cypress config: ', config)
+      console.log('cypress config: ', config);
       // implement node event listeners here
     },
   },
-})
+});
