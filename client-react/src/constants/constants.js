d1dbd2b756021c35dc3bac22eff0611d3b125bb4
ren132710 <ren132710@gmail.com>
2023-01-24 17:07:42 -0500

    remove fav prop

--- client-react/src/constants/constants.js
+++ client-react/src/constants/constants.js
@@ -10,28 +10,24 @@ export const DEFAULT_PLACES = [
     location: 'austin',
     lat: 30.2672,
     long: -97.7431,
-    favorite: true,
   },
   {
     id: '90f3d018-bbd3-45be-9c11-debbff73fb6c',
     location: 'san francisco',
     lat: 37.7749,
     long: -122.4194,
-    favorite: true,
   },
   {
     id: '6b819c6d-c8d4-4f2a-94c1-6eec48c6d8c8',
     location: 'montreal',
     lat: 45.5017,
     long: -73.5673,
-    favorite: true,
   },
   {
     id: 'c9ae7c46-81e4-4c9d-a933-bb3c8d14fc87',
     location: 'new york',
     lat: 40.7128,
     long: -74.006,
-    favorite: true,
   },
 ];
 
