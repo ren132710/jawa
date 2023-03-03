b0df86cfb05df5cbc45ac2877db3ac3a7182314a
ren132710 <ren132710@gmail.com>
2023-03-03 13:09:40 -0500

    return https

--- client-react/src/utils/iconUtils.js
+++ client-react/src/utils/iconUtils.js
@@ -6,7 +6,7 @@ export default function getIconUrl(iconKey, { size = 'small' } = {}) {
   if (size === 'small') sizeKey = '';
   if (size === 'large') sizeKey = '@4x';
   if (size === 'medium') sizeKey = '@2x';
-  return `http://openweathermap.org/img/wn/${iconKey}${sizeKey}.png`;
+  return `https://openweathermap.org/img/wn/${iconKey}${sizeKey}.png`;
 }
 
 /**
